a7f619b59811924165c9b296a56650359dd94240
shaik0123 <shaik.ismail8563@gmail.com>
2022-11-30 22:17:54 +0530

    ADDED:day-5 assignment

---
+++ EuclideanDistance.java
@@ -0,0 +1,10 @@
+package day5;
+
+public class EuclideanDistance {
+    public static void main(String[] args) {
+        int x = Integer.parseInt((args[0]));
+        int y = Integer.parseInt((args[1]));
+        double distance = Math.sqrt(x * x + y * y);
+        System.out.println("distance from  " +x+ " , " +y+ " to (0,0)" +" =" +distance);
+    }
+}
